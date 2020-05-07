22ecdc1d384e38d0824daa3e5fe002049ca680c8
daniela <1469@holbertonschool.com>
2020-05-07 21:05:55 +0000

    Adding index change in task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary.items(), key=lambda elem: elem[0])[0]
+        return max(a_dictionary.items(), key=lambda elem: elem[1])[0]
     return None
