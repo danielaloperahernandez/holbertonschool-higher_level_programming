86a822ac0f96b7d54fcabbfe9205dd2b94c47d70
daniela <1469@holbertonschool.com>
2020-05-07 19:19:33 +0000

    Adding exercises from 9 to 10

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    if a_dictionary:
+        for key in a_dictionary.keys():
+            new_dictionary[key] = a_dictionary[key] * 2
+    return new_dictionary
