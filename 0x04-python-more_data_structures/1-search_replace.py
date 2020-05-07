41a7e56fd9df0826192d5a6ecd3d8301d1255ec7
daniela <1469@holbertonschool.com>
2020-05-07 19:55:28 +0000

    Removing conditional for empty list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        return list(map(lambda el: replace if el == search else el, my_list))
+    return list(map(lambda el: replace if el == search else el, my_list))
