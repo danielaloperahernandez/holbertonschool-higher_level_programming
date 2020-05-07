53cbb193b7397f9b4d2977c3953695260917a488
daniela <1469@holbertonschool.com>
2020-05-07 19:50:11 +0000

    Fixing task one, logical failures for empty list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        return list(map(lambda elm: replace if elm == search
-                        else elm, my_list))
+        return list(map(lambda el: replace if el == search else el, my_list))
