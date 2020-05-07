d628f24b5c67b477121bc9feb23d92925310434d
daniela <1469@holbertonschool.com>
2020-05-07 15:35:41 +0000

    Addind README.md and excercises from 0 to 5

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        return list(map(lambda elm: replace if elm == search
+                    else elm, my_list))
