5df4a4cef9d6d58244270350273eec15ef76a454
daniela <1469@holbertonschool.com>
2020-05-07 18:52:54 +0000

    Adding changes for indentation and return

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     if my_list:
         return list(map(lambda elm: replace if elm == search
-                    else elm, my_list))
+                        else elm, my_list))
