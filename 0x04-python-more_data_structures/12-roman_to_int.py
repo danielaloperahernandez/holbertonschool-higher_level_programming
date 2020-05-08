6dcddbe2396703611795d94272b9876f02004f7a
daniela <1469@holbertonschool.com>
2020-05-08 01:00:42 +0000

    Changing name of dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
         ant = 0
         rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         for letter in reversed(roman_string):
-            num = romans[letter]
+            num = rom[letter]
             num = num if num >= ant else -num
             sum_roma += num
             ant = num
