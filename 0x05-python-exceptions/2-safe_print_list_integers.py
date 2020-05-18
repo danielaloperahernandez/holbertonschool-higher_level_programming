249789cb57d94d71d1cb3b75fcc3b59cf2b38c3a
daniela <1469@holbertonschool.com>
2020-05-18 17:25:22 +0000

    Fixing logical failures

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (IndexError, TypeError):
-            return count
-        else:
             count += 1
+        except (IndexError, TypeError):
+            continue
     print()
     return (count)
