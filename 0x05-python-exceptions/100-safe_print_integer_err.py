ae124d7cfd4095346d1dd431a1c35541d0611d73
daniela <1469@holbertonschool.com>
2020-05-18 18:25:16 +0000

    Fixing logical failures

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    import sys
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as error:
-        sys.stderr.write("Exception: {}".format(error))
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
     else:
         return True
