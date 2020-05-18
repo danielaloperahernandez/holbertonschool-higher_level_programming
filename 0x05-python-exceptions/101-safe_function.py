c278a9aa2ddbfcc4075b971d1883eddca1641656
daniela <1469@holbertonschool.com>
2020-05-18 18:49:27 +0000

    Changing the way it prints in standard error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
-    import sys
     try:
         result = fct(*args)  # passing arguments to function
         return result
     except Exception as error:
-        sys.stderr.write("Exception: {}".format(error))
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
