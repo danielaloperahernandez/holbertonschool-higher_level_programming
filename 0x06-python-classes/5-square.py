bfdb3a9ebf165a35321edab946e0753ad5be9ff1
daniela <1469@holbertonschool.com>
2020-05-19 18:48:28 +0000

    Adding condition for print space when size is 0

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -21,5 +21,9 @@ class Square:
         return(self.__size**2)
 
     def my_print(self):
-        for i in range(self.__size):
-            print("#" * self.__size)
+        if self.__size is 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
