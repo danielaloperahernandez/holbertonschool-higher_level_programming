fbc16d9537ac7ec3b1d982282d03e418cb863699
daniela <1469@holbertonschool.com>
2020-05-19 21:57:51 +0000

    Fixing indentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,9 +31,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (type(value) is not tuple or len(value) is not 2 or
-                any(map(lambda i: type(i) is not int or i < 0, value))):
-                raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value) is not tuple or len(value) is not 2 or \
+           type(value[0] is not int or value[0] < 0) or \
+           type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
