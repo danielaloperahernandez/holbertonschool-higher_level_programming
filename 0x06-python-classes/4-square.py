b7791d570f1e63763d863c61b3d06a89c82c6599
daniela <1469@holbertonschool.com>
2020-05-19 21:06:01 +0000

    Fixing comments for task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"Defines a class Square"
+
+
 class Square:
-    """Class Square that defines a square"""
+    """Class Square that defines a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
         self.size = size
 
