721ba60e0882f649f993095e03340ac8827f988f
daniela <1469@holbertonschool.com>
2020-05-26 01:26:56 +0000

    Changing conditionals

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
 
     def perimeter(self):
         """returns the perimeter"""
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         return (self.width * 2) + (self.height * 2)
 
