0279e53710ce76f36df11ef0456fe8adc6f75488
daniela <1469@holbertonschool.com>
2020-05-26 01:48:49 +0000

    Deleting unnecessary space

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,4 +59,4 @@ class Rectangle:
 
     def __repr__(self):
         """return a string representation for reproduction"""
-        return "Rectangle ({:d}, {:d})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
