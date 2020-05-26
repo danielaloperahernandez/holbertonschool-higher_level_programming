99c93258dec91962e64b07aa664196c5ca3d965a
daniela <1469@holbertonschool.com>
2020-05-26 00:35:17 +0000

    Adding changes for checker

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -11,20 +11,6 @@ class Rectangle:
         self.width = width
 
     @property
-    def width(self):
-        """getter for the private instance attribute width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """setter for the private instance attribute width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
     def height(self):
         """getter for the private instance attribute height"""
         return self.__height
@@ -38,6 +24,20 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     def area(self):
         """returns the area"""
         return (self.__width * self.__height)
@@ -46,7 +46,7 @@ class Rectangle:
         """returns the perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return ((self.__width * 2) + (self.__height * 2))
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         """returns printable string (representation og the rectangle)"""
