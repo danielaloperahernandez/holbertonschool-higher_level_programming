d91fb23f410938073288e7c3164776c631118d88
daniela <1469@holbertonschool.com>
2020-06-03 23:05:23 +0000

    Fixing task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Method that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as my_file_0:
-        print(my_file_0.read())
+        print(my_file_0.read(), end="")
