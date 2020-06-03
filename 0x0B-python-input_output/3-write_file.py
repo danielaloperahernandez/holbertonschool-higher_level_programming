816edd2f60069a19748cce00a78c906ae85fdde6
daniela <1469@holbertonschool.com>
2020-06-03 23:52:19 +0000

    Fixing task 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
     Method that writes a string to a text file (UTF8)
     and returns the number of characters written
     """
-    with open(filename, mode="r+", encoding="utf-8") as my_file_3:
+    with open(filename, mode="w", encoding="utf-8") as my_file_3:
         return my_file_3.write(text)
