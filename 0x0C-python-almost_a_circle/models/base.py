9f11bde252388e22ab31756d8f5945a45cb2c517
daniela <1469@holbertonschool.com>
2020-06-09 02:56:50 +0000

    Adding all task for the project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+"""Module for Base class"""
+from json import dumps, loads
+import csv
+from os import path
+
+
+class Base:
+    """This is a “base” class of all other classes in this project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+        if id is None:
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
+        self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if not list_dictionaries or not len(list_dictionaries):
+            list_dictionaries = []
+        return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a fil"""
+        if not list_objs:
+            list_objs = []
+        list_objs = [obj.to_dictionary() for obj in list_objs]
+        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new = cls(1)
+        else:
+            new = None
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances (convert json representations)"""
+        file_load = "{}.json".format(cls.__name__)
+        if not path.isfile(file_load):
+            return []
+        with open(file_load, "r") as f:
+            return[cls.create(**dic) for dic in cls.from_json_string(f.read())]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        if not list_objs:
+            list_objs = []
+        with open("{}.csv".format(cls.__name__), 'w') as file_csv:
+            if cls.__name__ == "Rectangle":
+                fields = ['id', 'width', 'height', 'x', 'y']
+            elif cls.__name__ == "Square":
+                fields = ['id', 'size', 'x', 'y']
+            writer = csv.DictWriter(file_csv, fieldnames=fields)
+            for obj in list_objs:
+                writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        with open("{}.csv".format(cls.__name__), 'r') as file_csv:
+            if cls.__name__ == "Rectangle":
+                fields = ['id', 'width', 'height', 'x', 'y']
+            elif cls.__name__ == "Square":
+                fields = ['id', 'size', 'x', 'y']
+            reader = csv.DictReader(file_csv)
+            list_objs = []
+            for row in reader:
+                for key in row:
+                    row[key] = int(row[key])
+                list_objs.append(cls.create(**row))
+            return list_objs
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        import turtle
+        import time
+        from random import randrange
+        turtle.Screen().colormode(255)
+        for i in list_rectangles + list_squares:
+            t = turtle.Turtle()
+            t.color((randrange(255), randrange(255), randrange(255)))
+            t.pensize(1)
+            t.penup()
+            t.pendown()
+            t.setpos((i.x + t.pos()[0], i.y - t.pos()[1]))
+            t.pensize(10)
+            t.forward(i.width)
+            t.left(90)
+            t.forward(i.height)
+            t.left(90)
+            t.forward(i.width)
+            t.left(90)
+            t.forward(i.height)
+            t.left(90)
+            t.end_fill()
+
+        time.sleep(5)
+
