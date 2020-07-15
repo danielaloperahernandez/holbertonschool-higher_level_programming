91135ca848811a2a3afbfe293a5c8bce648d4bce
daniela <1469@holbertonschool.com>
2020-07-15 00:10:31 +0000

    Fixing task 5

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title AS title
        FROM tv_shows
        INNER JOIN tv_show_genres
-       ON tv_show.id = tv_show_genres.genre_id
+       ON tv_shows.id = tv_show_genres.genre_id
        INNER JOIN tv_genres
        ON tv_genres.id = tv_show_genres.show_id
        WHERE tv_genres.name = "Comedy"
