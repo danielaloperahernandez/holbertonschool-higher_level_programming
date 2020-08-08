9927aab361dd534f21ac8cdb0a9f2349205731ef
daniela <1469@holbertonschool.com>
2020-08-08 00:37:12 +0000

    Adding tasks from 0 to 102

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists states"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class representing the states table"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="cities")
