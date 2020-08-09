6495314fae25026c14fbea2d135f4c6d5b3cafd6
daniela <1469@holbertonschool.com>
2020-08-09 21:19:43 +0000

    Fixing task 7

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from model_city import City
 
 Base = declarative_base()
 
@@ -11,9 +12,6 @@ Base = declarative_base()
 class State(Base):
     """Class representing the states table"""
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="cities")
