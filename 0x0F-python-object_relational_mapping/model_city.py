840325b396b3094ebf7f4d34048b6d0d5b467d51
daniela <1469@holbertonschool.com>
2020-08-09 21:05:00 +0000

    Fixing task 6

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,6 +11,7 @@ class City(Base):
     """Class representing the states table"""
     __tablename__ = 'cities'
 
-    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from model_city import City
 
 Base = declarative_base()
 
