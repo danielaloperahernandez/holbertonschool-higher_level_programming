1625b739c146dcb17c3ebf94bd45e8cd6df4f1bf
daniela <1469@holbertonschool.com>
2020-08-09 21:26:41 +0000

    Fixing task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,15 +7,14 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(argv[1], argv[2],
                 argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).\
-            filter(State.name.like('%a%')).all():
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
         session.delete(state)
     session.commit()
-    print(new_state.id)
     session.close()
