e509ae0b73edad98d041fdb7c187b40114553a0d
daniela <1469@holbertonschool.com>
2020-08-24 20:37:38 +0000

    Fixing task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # script that fetches https://intranet.hbtn.io/status
-import requests
+from requests import get
 
 if __name__ == "__main__":
 
-    req = requests.get('https://intranet.hbtn.io/status')
+    req = get('https://intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}"
-        .format(type(req.text), req.text))
+          .format(type(req.text), req.text))
