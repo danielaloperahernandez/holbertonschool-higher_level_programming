98a1958765ea38b45159a573fac86d5b88ed7e63
daniela <1469@holbertonschool.com>
2020-08-24 20:30:16 +0000

    Adding tasks 4 and 5

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# script that fetches https://intranet.hbtn.io/status
+from requests import get
+
+if __name__ == "__main__":
+
+try:
+    req = get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(req.text), req.text))
+except Exception as e:
+    print(e)
