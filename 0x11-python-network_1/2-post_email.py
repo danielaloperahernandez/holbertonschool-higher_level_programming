bc356455d0672f58ebd009d8ce61b16e4a4b31e6
daniela <1469@holbertonschool.com>
2020-08-24 19:45:23 +0000

    Fixing task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,13 +7,12 @@ the email as a parameter, and displays the body of the response
 
 if __name__ == "__main__":
     import urllib.request
+    import urllib.request.parse
     import urllib.error
     import sys
 
-values = {}
-values['email'] = sys.argv[2]
 try:
-    data = urllib.parse.urlencode(values).encode()
+    data = urllib.parse.urlencode({'email': argv[2]}).encode()
     with urllib.request.urlopen(sys.argv[1], data) as res:
         print(res.read().decode('utf-8'))
 except (urllib.error.URLError, IndexError) as e:
