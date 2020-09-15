deb3cb1a2e40fe7bd704a86360d8f179444d4541
danielaloperahernandez <danielaloperahernandez@gmail.com>
2020-09-15 19:41:16 +0000

    Fixing task 7 (Adding negative case)

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let Integer = parseInt(process.argv[2], 10);
-if (Number.isInteger(Integer) && Integer > 0) {
+if (!isNaN(Integer) && Integer > 0) {
   for (Integer; Integer !== 0; Integer--) {
     console.log('C is fun');
   }
-} else {
+} else if (isNaN(Integer)) {
   console.log('Missing number of occurrences');
 }
