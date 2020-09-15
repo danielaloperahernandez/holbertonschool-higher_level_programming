cac367357253c68b02e6edff78d6fe8349938cb3
danielaloperahernandez <danielaloperahernandez@gmail.com>
2020-09-15 19:30:46 +0000

    Fixing task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Integer = parseInt(process.argv[2], 10);
-if (Integer) {
+if (!isNaN(Integer)) {
   console.log('My Number: ' + Integer);
 } else {
   console.log('Not a number');
