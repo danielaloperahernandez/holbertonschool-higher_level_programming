6153ebf121ea009c88fe9a8dba599e1cee827ce8
danielaloperahernandez <danielaloperahernandez@gmail.com>
2020-09-15 19:43:55 +0000

    Fixing task 8 (Using NaN inside conditionals)

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const Integer = parseInt(process.argv[2], 10);
 let i = Integer;
-if (Number.isInteger(Integer) && Integer > 0) {
+if (!isNaN(Integer) && Integer > 0) {
   for (i; i !== 0; i--) {
-      console.log('X'.repeat(Integer));
+    console.log('X'.repeat(Integer));
   }
-} else if (!Number.isInteger(Integer)) {
+} else if (isNaN(Integer)) {
   console.log('Missing size');
 }
